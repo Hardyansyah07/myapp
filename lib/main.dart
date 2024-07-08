45c0e3f3b5a98476b4c8eb85a9cda51ca6c2b08d
hardyansyahmaulanasidik299 <hardyansyahmaulanasidik299@gmail.com>
2024-07-08 02:25:38 +0000

    belajar flutter

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? Key}) : super(key: Key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('Belajar Fluuter'),
+          centerTitle: true,
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: Center(
+          child: Text('Hello World',
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          ),
+        ),
+      ),
+    );
+  }
+}
